a3182595e79b361bedef37e69953444067e7cf31
Yingqiang Ge <gyq1101@gmail.com>
2023-04-09 20:39:53 -0400

    upload dataloader, combinator

--- flan_t5_finetune.py
+++ flan_t5_finetune.py
@@ -1,3 +1,25 @@
+""" 
+This code mainly used to fine-tune Flan-T5.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = "Wenyue Hua, Yingqiang Ge"
+__copyright__ = "Copyright 2023, OpenAGI"
+__date__ = "2023/04/09"
+__license__ = "GPLv3"
+__version__ = "0.0.1"
+
+
+
 import torch
 from torch import optim
 from torch.optim import optimizer
