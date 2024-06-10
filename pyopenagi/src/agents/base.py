fffb2004174ac14974a0aef8a6dc65a7c6f721ed
dongyuanjushi <kai.mei@rutgers.edu>
2024-06-10 16:47:51 -0700

    chore: update workflow for building pyopenagi from local and pypi

---
+++ pyopenagi/src/agents/base.py
@@ -0,0 +1,183 @@
+import os
+
+import json
+
+from .agent_process import (
+    AgentProcess,
+    # AgentProcessQueue
+)
+
+import logging
+
+import time
+
+from threading import Thread
+
+from datetime import datetime
+
+import numpy as np
+
+from ..utils.logger import AgentLogger
+
+from ..utils.message import Message
+class CustomizedThread(Thread):
+    def __init__(self, target, args=()):
+        super().__init__()
+        self.target = target
+        self.args = args
+        self.result = None
+
+    def run(self):
+        self.result = self.target(*self.args)
+
+    def join(self):
+        super().join()
+        return self.result
+
+class BaseAgent:
+    def __init__(self,
+                 agent_name,
+                 task_input,
+                 llm,
+                 agent_process_queue,
+                 agent_process_factory,
+                 log_mode: str
+        ):
+        self.agent_name = agent_name
+        self.config = self.load_config()
+        self.prefix = " ".join(self.config["description"])
+        self.task_input = task_input
+        self.llm = llm
+        self.agent_process_queue = agent_process_queue
+        self.agent_process_factory = agent_process_factory
+
+        self.log_mode = log_mode
+        self.logger = self.setup_logger()
+        self.logger.log(f"Initialized. \n", level="info")
+
+        self.set_status("active")
+        self.set_created_time(time.time())
+
+    def run(self):
+        '''Execute each step to finish the task.'''
+        pass
+
+    def setup_logger(self):
+        logger = AgentLogger(self.agent_name, self.log_mode)
+        return logger
+
+    def load_config(self):
+        script_path = os.path.abspath(__file__)
+        script_dir = os.path.dirname(script_path)
+        config_file = os.path.join(script_dir, "agent_config/{}.json".format(self.agent_name))
+        with open(config_file, "r") as f:
+            config = json.load(f)
+            return config
+
+    def get_response(self,
+            message,
+            temperature=0.0
+        ):
+        thread = CustomizedThread(target=self.query_loop, args=(message, ))
+        thread.start()
+        return thread.join()
+
+    def query_loop(self, message):
+        agent_process = self.create_agent_request(message)
+
+        completed_response, start_times, end_times, waiting_times, turnaround_times = "", [], [], [], []
+
+        while agent_process.get_status() != "done":
+            thread = Thread(target=self.listen, args=(agent_process, ))
+            current_time = time.time()
+            # reinitialize agent status
+            agent_process.set_created_time(current_time)
+            agent_process.set_response(None)
+            self.agent_process_queue.put(agent_process)
+
+            thread.start()
+            thread.join()
+
+            completed_response = agent_process.get_response()
+            if agent_process.get_status() != "done":
+                self.logger.log(
+                    f"Suspended due to the reach of time limit ({agent_process.get_time_limit()}s). Current result is: {completed_response.response_message}\n",
+                    level="suspending"
+                )
+            start_time = agent_process.get_start_time()
+            end_time = agent_process.get_end_time()
+            waiting_time = start_time - agent_process.get_created_time()
+            turnaround_time = end_time - agent_process.get_created_time()
+
+            start_times.append(start_time)
+            end_times.append(end_time)
+            waiting_times.append(waiting_time)
+            turnaround_times.append(turnaround_time)
+            # Re-start the thread if not done
+
+        self.agent_process_factory.deactivate_agent_process(agent_process.get_pid())
+
+        return completed_response, start_times, end_times, waiting_times, turnaround_times
+
+    def create_agent_request(self, message):
+        agent_process = self.agent_process_factory.activate_agent_process(
+            agent_name = self.agent_name,
+            message = message
+        )
+        agent_process.set_created_time(time.time())
+        # print("Already put into the queue")
+        return agent_process
+
+    def listen(self, agent_process):
+        """Response Listener for agent
+
+        Args:
+            agent_process (AgentProcess): Listened AgentProcess
+
+        Returns:
+            str: LLM response of Agent Process
+        """
+        while agent_process.get_response() is None:
+            time.sleep(0.2)
+
+        return agent_process.get_response()
+
+    def set_aid(self, aid):
+        self.aid = aid
+
+    def get_aid(self):
+        return self.aid
+
+    def get_agent_name(self):
+        return self.agent_name
+
+    def set_status(self, status):
+
+        """
+        Status type: Waiting, Running, Done, Inactive
+        """
+        self.status = status
+
+    def get_status(self):
+        return self.status
+
+    def set_created_time(self, time):
+        self.created_time = time
+
+    def get_created_time(self):
+        return self.created_time
+
+    def set_start_time(self, time):
+        self.start_time = time
+
+    def get_start_time(self):
+        return self.start_time
+
+    def set_end_time(self, time):
+        self.end_time = time
+
+    def get_end_time(self):
+        return self.end_time
+
+    def parse_result(self, prompt):
+        pass
--- setup.py
+++ setup.py
@@ -8,23 +8,7 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name='openagi',
-    version='0.0.1',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/agiresearch",
-    packages=['openagi'],
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Machine Learning",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3 :: Only",
-    ],
-    python_requires=">=3.7, <4",
+    name='pyopenagi',
+    version='0.0.3',
+    packages=find_packages()
 )
